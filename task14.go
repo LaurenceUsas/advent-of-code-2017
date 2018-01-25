a59a0d6cea2c7b75f7bb023cf22fe1ab2450f0c0
Laurynas Usas <demox@Laurynass-MacBook-Pro.local>
2018-01-25 22:11:31 +0200

    Solution Task 14

---
+++ task14.go
@@ -0,0 +1,18 @@
+package main
+
+//Task14 Solution
+func Task14() {
+	//read input.
+	// pwd, _ := os.Getwd()
+	// file := helpers.InputFile(pwd + "/input/input14.txt")
+	//solve part 1
+	//solve part 2
+}
+
+func task14PartOne() {
+
+}
+
+func task14PartTwo() {
+
+}
